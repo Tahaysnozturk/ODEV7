a0bc8c95cbbd96396cef44772f16e2161f93a377
tiyol <tiyol@LAPTOP-6IPT3PBJ>
2024-01-07 00:23:33 +0300

    Proje dosyası ekle.

---
+++ ODEV7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Bir ikili sayı girin: ");
+        string ikiliSayi = Console.ReadLine();
+
+        int ondalikDeger = 0;
+        int tabanDeger = 1;
+
+        for (int i = ikiliSayi.Length - 1; i >= 0; i--)
+        {
+            if (ikiliSayi[i] == '1')
+            {
+                ondalikDeger += tabanDeger;
+            }
+            tabanDeger *= 2;
+        }
+
+        Console.WriteLine("Girilen ikili (Binary) Sayı: " + ikiliSayi);
+        Console.WriteLine("Ondalık (Decimal) Sayı: " + ondalikDeger);
+    }
+}
